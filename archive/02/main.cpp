27275cd63fd01f7021f48f62ad7eb2736a552d59
43-21 <lasse.plogsties@outlook.de>
2024-12-21 00:18:33 +0100

    archive 02

---
+++ archive/02/main.cpp
@@ -0,0 +1,90 @@
+#include <Adafruit_NeoPixel.h>
+#define PIN 15
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, PIN, NEO_GRB + NEO_KHZ800);
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+void stars(int count, uint8_t wait){
+  /*TODO: 
+   * Implementiere hier den stars Effekt.
+   * Dabei sollen nacheinander <count> viele LEDs gelb aufleuchten und langsam ausdimmen (die Geschwindigkeit wird von wait beeinflusst) bevor die nächste LED aufleuchtet. 
+   */
+  srand(time(NULL));
+  strip.clear();
+  for (int i = 0; i < count; i++) {
+    int pix = rand() % strip.numPixels();
+    strip.setPixelColor(pix, 255, 255, 0);
+    for (int j = 255; j > 0; j--) {
+      strip.setBrightness(j);
+      strip.show();
+      delay(wait);
+    }
+    strip.setPixelColor(pix, 0, 0, 0);
+    strip.setBrightness(255);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+void setup() {
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+  // Some example procedures showing how to display to the pixels:
+  colorWipe(strip.Color(255, 0, 0), 50); // Red
+  colorWipe(strip.Color(0, 255, 0), 50); // Green
+  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+
+  stars(20, 2);
+
+  rainbow(20);
+  rainbowCycle(20);
+}
